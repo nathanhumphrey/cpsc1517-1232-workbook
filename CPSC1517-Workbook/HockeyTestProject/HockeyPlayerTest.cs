bc79cc694f5f4d5167a1e6e486002e45fb0b976e
Nathan Humphrey <NathanH@nait.ca>
2024-01-17 17:46:47 -0700

    Add test project

---
+++ CPSC1517-Workbook/HockeyTestProject/HockeyPlayerTest.cs
@@ -0,0 +1,21 @@
+using FluentAssertions;
+using Hockey.Data;
+
+namespace HockeyTestProject
+{
+	public class HockeyPlayerTest
+	{
+		//[Fact]
+		//public void HockeyPlayer_DefaultConstructor_CreatesGoodPlayer()
+		//{
+		//	// arrange
+		//	HockeyPlayer player;
+
+		//	// act
+		//	player = new HockeyPlayer();
+
+		//	//assert
+		//	player.Should().BeOfType<HockeyPlayer>();
+		//}
+	}
+}
