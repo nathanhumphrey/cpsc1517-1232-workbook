087add716b761f86bfb9425a2c69045310c00905
Nathan Humphrey <NathanH@nait.ca>
2024-03-04 14:18:21 -0700

    Add WestWindSystem and EFCoreTestApp

---
+++ CPSC1517-Workbook/EFCoreTestApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using WestWindSystem;
+using WestWindSystem.Models;
+
+WestWindContext context = new WestWindContext();
+
+context.Customers.Select(a => a);
+
+foreach (var customer in context.Customers)
+{
+	Console.WriteLine(customer.ContactName);
+}
